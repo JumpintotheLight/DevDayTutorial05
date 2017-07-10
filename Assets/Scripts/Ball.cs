03bdfff9c5e4babc535c1e1caa828276cee33c63
Sky Rolnick <skymeson@gmail.com>
2017-07-10 18:15:50 -0400

    Dev Day Tutorial 5
    First Commit - Block Breaker VR

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour{
+    private Paddle paddle;
+    private Vector3 paddleToBallVector;
+
+    private bool hasStarted = false;
+
+	// Use this for initialization
+	void Start () {
+        paddle = GameObject.FindObjectOfType<Paddle>();
+        paddleToBallVector = this.transform.position - paddle.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (!hasStarted)
+        {
+            // Lock the ball relative to paddle
+            this.transform.position = paddle.transform.position + paddleToBallVector;
+
+            //Starts game and gives ball velocity...waits for mouse press
+            if (Input.GetMouseButtonDown(0))
+            {
+                hasStarted = true;
+                this.GetComponent<Rigidbody2D>().velocity = new Vector2(2f, 8f);
+            }
+        }
+	}
+
+    void OnCollisionEnter2D(Collision2D collision) {
+        Vector2 tweak = new Vector2(Random.Range(0f, 0.2f), Random.Range(0f, 0.1f));
+        this.GetComponent<Rigidbody2D>().velocity += tweak;
+    }
+}
---
+++ Assets/Scripts/LoseCollider.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LoseCollider : MonoBehaviour {
+
+    private Level_Manager levelManager;
+
+
+    void Start() {
+        levelManager = GameObject.FindObjectOfType<Level_Manager>();
+    }
+	void OnTriggerEnter2D (Collider2D trigger) {
+         levelManager.LoadLevel("Lose");
+    }
+	
+	void OnCollisionEnter2D (Collision2D collision){
+	}
+}
